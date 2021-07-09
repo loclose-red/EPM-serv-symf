e84df147dba1f108dc4394c2373c72b16dca80af
Gerard Le Forban <gerard.leforban@free.fr>
2021-07-09 18:11:42 +0200

    Intégration page adminBoard

--- src/Controller/Admin/AdminBoardController.php
+++ src/Controller/Admin/AdminBoardController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\EquipementRepository;
+use App\Repository\SiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,10 +14,25 @@ class AdminBoardController extends AbstractController
     /**
      * @Route("/adminboard", name="admin_board")
      */
-    public function index(): Response
+    public function index(SiteRepository $siteRepository, Request $request, EquipementRepository $equipementRepository): Response
     {
+        $equipements = [];
+        $siteSelect = [];
+
+        if ($request->get('select_site')) {
+            $siteId = $request->get('select_site');
+            $siteSelect = $siteRepository->findBy(["id" => $siteId]);
+            $equipements = $equipementRepository->findBy(["site" => $siteSelect]);
+            // dd($equipements);
+        } else {
+            $siteSelect = $siteRepository->findBy([]);
+        }
+        $allSites = $siteRepository->findAll();
+        // dd($siteSelect);
         return $this->render('admin/adminboard.html.twig', [
-            'controller_name' => 'AdminBoardController',
+            'sites' => $allSites,
+            'siteSelect' => $siteSelect,
+            'equipements' => $equipements,
         ]);
     }
 }
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,6 +9,17 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(): Response
+    {
+
+
+        return $this->render('home.html.twig');
+    }
+
     /**
      * @Route("/login", name="app_login")
      */
