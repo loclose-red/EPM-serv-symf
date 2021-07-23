08c370736e79e05d787d24a30fc2668ce35a08cf
Gerard Le Forban <gerard.leforban@free.fr>
2021-07-23 16:34:29 +0200

    Crud EasyAdmin ajout choix "grandeur" (unite de mesure) pour un capteur.

--- src/Controller/Admin/CapteurCrudController.php
+++ src/Controller/Admin/CapteurCrudController.php
@@ -3,8 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Capteur;
+use App\Entity\Grandeur;
+use App\Form\UtilisateurType;
+use App\Repository\GrandeurRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -15,16 +19,36 @@ class CapteurCrudController extends AbstractCrudController
         return Capteur::class;
     }
 
+    //Création d'une fonction pour récupérer la liste des "grandeurs"
+    //cette fonction sera utilisée pour afficher la liste des choix de grandeur pour un catpeur
+    public function listeGrandeurs()
+    {
+        $tableauRetour = [];
+        $grandeurRepository= $this->getDoctrine()->getRepository(Grandeur::class);
+        $tableauListe = $grandeurRepository->findAll();
+        foreach($tableauListe as $uneGrandeur)
+            $tableauRetour[$uneGrandeur->getGraUnite()] = $uneGrandeur;
+        return $tableauRetour;
+    }
+
     
+
     public function configureFields(string $pageName): iterable
     {
         return [
             TextField::new('cap_marque', 'Marque'),
             TextField::new('cap_modele', 'Modèle'),
             TextField::new('cap_serie', 'N° série'),
+            //pour afficher la grandeur dans l'index
             TextField::new('grandeur', 'Unité')->onlyOnIndex(),
+            //pour afficher la liste des grandeurs dans l'édition
+            ChoiceField::new('grandeur', 'Unité')
+            ->setChoices($this->listeGrandeurs())
+            ->hideOnIndex(),
             TextareaField::new('cap_information', 'Information'),
-            BooleanField::new('cap_archive', 'Archive')
+            BooleanField::new('cap_archive', 'Archive'),
+                    
+                                
         ];
     }
     
