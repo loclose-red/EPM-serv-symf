bc84761b521fc5cbcbe45180a90f0b0b13e4cda5
Gerard Le Forban <gerard.leforban@free.fr>
2021-07-30 22:54:34 +0200

    récupération des id des pt de mesures dans fichier js

--- public/js/chart_JS.js
+++ public/js/chart_JS.js
@@ -1,1 +1,19 @@
 console.log('dans fichier js')
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Select elements by their data attribute
+    const entryElements =
+        document.querySelectorAll('[data-entry-id]');
+
+    // Map over each element and extract the data value
+    const entryIds =
+        Array.from(entryElements).map(
+            item => item.dataset.entryId
+        );
+
+    // You'll now have an array containing string values
+    console.log("message js externe3: " + entryIds); // eg: ["1", "2", "3"]
+});
