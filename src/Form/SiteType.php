e3a3885ac03e14ce9f927e5cd7a7350ecc146aaa
Gerard Le Forban <gerard.leforban@free.fr>
2021-09-10 17:46:29 +0200

    creation crud site

---
+++ src/Form/SiteType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Site;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SiteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('sit_raison_sociale')
+            ->add('sit_ville')
+            ->add('sit_c_postal')
+            ->add('sit_adresse')
+            ->add('sit_information')
+            ->add('sit_archive')
+            ->add('utilisateurs')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Site::class,
+        ]);
+    }
+}
