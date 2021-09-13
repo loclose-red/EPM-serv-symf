38ce5d639429d920f28a123032e1e31bf48774c0
Gerard Le Forban <gerard.leforban@free.fr>
2021-09-13 15:32:53 +0200

    Formulaire contact :  type textarea pour message, et nom des champs modifiés.

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -3,6 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Contact;
+// use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,10 +17,10 @@ class ContactType extends AbstractType
         $builder
             ->add('nom')
             ->add('prenom')
-            ->add('societe')
+            ->add('societe', TextType::class, ['label' => 'Société'])
             ->add('email')
-            ->add('phone')
-            ->add('message')
+            ->add('phone', TextType::class, ['label' => 'Téléphone'])
+            ->add('message' , TextareaType::class)
         ;
     }
 
