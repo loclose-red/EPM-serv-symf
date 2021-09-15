bfa04d7b30a46caf4802b9a4b4d68233b0aedb75
Gerard Le Forban <gerard.leforban@free.fr>
2021-09-15 15:11:07 +0200

    mise en forme crud twig 'capteur'

--- src/Form/CapteurType.php
+++ src/Form/CapteurType.php
@@ -12,14 +12,14 @@ class CapteurType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('cap_marque')
-            ->add('cap_modele')
-            ->add('cap_serie')
-            ->add('cap_information')
-            ->add('cap_archive')
-            ->add('cap_macadress')
-            ->add('grandeur')
-            ->add('site')
+            ->add('cap_marque',null,['label'=>'Marque'])
+            ->add('cap_modele',null,['label'=>'Modèle'])
+            ->add('cap_serie',null,['label'=>'N° Série'])
+            ->add('cap_macadress',null,['label'=>'Adresse mac (BT)'])
+            ->add('grandeur',null,['label'=>'Grandeur'])
+            ->add('cap_information',null,['label'=>'Information'])
+            ->add('site',null,['label'=>'Site'])
+            ->add('cap_archive',null,['label'=>'Archiver'])
         ;
     }
 
